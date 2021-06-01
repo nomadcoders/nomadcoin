cb9b8ed087584c3e8219bef79003e26baf52b071
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-01 17:03:50 +0900

    2.1 Our First Block

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
 
 func main() {
-	fmt.Println("Welcome to Nomadcoin!")
+	genesisBlock := block{"Genesis Block", "", ""}
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+	fmt.Println(genesisBlock)
 }
