886f06286123b1d7d4f5d217ba69dd4f9bed990c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-02 23:36:09 +0900

    3.0 Setup

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/nomadcoders/nomadcoin/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello from home!")
+}
+
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
