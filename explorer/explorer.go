93574527eb1bcb04c75615a906adf3fb153d27d7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-03 21:43:26 +0900

    3.5 Refactoring

---
+++ explorer/explorer.go
@@ -0,0 +1,48 @@
+package explorer
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/nomadcoders/nomadcoin/blockchain"
+)
+
+const (
+	port        string = ":4000"
+	templateDir string = "explorer/templates/"
+)
+
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
--- main.go
+++ main.go
@@ -1,48 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"github.com/nomadcoders/nomadcoin/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
+import "github.com/nomadcoders/nomadcoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
