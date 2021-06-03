4a4883ff3f1b4980c5ec203d32e524f94eb2734e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-03 16:01:06 +0900

    3.2 Rendering Blocks

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	tmpl := template.Must(template.ParseFiles("templates/home.gohtml"))
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
 	tmpl.Execute(rw, data)
 }
