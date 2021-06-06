23be35afc69dc02bfdc820624ca774bd57d1e62a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-06 17:06:26 +0900

    5.1 Parsing Commands

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"github.com/nomadcoders/nomadcoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer:	Start the HTML Explorer\n")
+	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
+
 }
