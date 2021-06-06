a901465559edcc370ae6dd83a6c86c6a11ded859
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-07 00:11:54 +0900

    5.3 Flag

---
+++ cli/cli.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/nomadcoders/nomadcoin/explorer"
+	"github.com/nomadcoders/nomadcoin/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n\n")
+	fmt.Printf("Please use the following flags:\n\n")
+	fmt.Printf("-port:		Set the PORT of the server\n")
+	fmt.Printf("-mode:		Choose between 'html' and 'rest'\n\n")
+	os.Exit(0)
+}
+
+func Start() {
+	if len(os.Args) == 1 {
+		usage()
+	}
+
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+}
--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welcome to 노마드 코인\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer:	Start the HTML Explorer\n")
-	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
-	os.Exit(0)
-}
+import "github.com/nomadcoders/nomadcoin/cli"
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
+	cli.Start()
 }
