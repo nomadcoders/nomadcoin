ca70c8f7d762f3d09b1db5fa3b09b7736249c927
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-08 14:48:52 +0900

    6.1 Creating the Database

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"github.com/boltdb/bolt"
+	"github.com/nomadcoders/nomadcoin/utils"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		db = dbPointer
+		utils.HandleErr(err)
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleErr(err)
+	}
+	return db
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/nomadcoders/nomadcoin
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
+)
