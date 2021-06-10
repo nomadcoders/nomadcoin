fc901c8c1eabb1111ada8ddca18b3bb319325ee9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-10 19:12:48 +0900

    7.4 Difficulty part Two

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -10,6 +10,8 @@ import (
 const (
 	defaultDifficulty  int = 2
 	difficultyInterval int = 5
+	blockInterval      int = 2
+	allowedRange       int = 2
 )
 
 type blockchain struct {
@@ -33,6 +35,7 @@ func (b *blockchain) AddBlock(data string) {
 	block := createBlock(data, b.NewestHash, b.Height+1)
 	b.NewestHash = block.Hash
 	b.Height = block.Height
+	b.CurrentDifficulty = block.Difficulty
 	b.persist()
 }
 
@@ -51,11 +54,25 @@ func (b *blockchain) Blocks() []*Block {
 	return blocks
 }
 
+func (b *blockchain) recalculateDifficulty() int {
+	allBlocks := b.Blocks()
+	newestBlock := allBlocks[0]
+	lastRecalculatedBlock := allBlocks[difficultyInterval-1]
+	actualTime := (newestBlock.Timestamp / 60) - (lastRecalculatedBlock.Timestamp / 60)
+	expectedTime := difficultyInterval * blockInterval
+	if actualTime <= (expectedTime - allowedRange) {
+		return b.CurrentDifficulty + 1
+	} else if actualTime >= (expectedTime + allowedRange) {
+		return b.CurrentDifficulty - 1
+	}
+	return b.CurrentDifficulty
+}
+
 func (b *blockchain) difficulty() int {
 	if b.Height == 0 {
 		return defaultDifficulty
 	} else if b.Height%difficultyInterval == 0 {
-		// recalculate the difficulty
+		return b.recalculateDifficulty()
 	} else {
 		return b.CurrentDifficulty
 	}
