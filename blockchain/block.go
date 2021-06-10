acd810178fa4af060ed62967ad525b492506f9d2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-10 15:41:54 +0900

    7.1 PoW Proof Of Concept

--- blockchain/block.go
+++ blockchain/block.go
@@ -9,6 +9,8 @@ import (
 	"github.com/nomadcoders/nomadcoin/utils"
 )
 
+const difficulty int = 2
+
 type Block struct {
 	Data       string `json:"data"`
 	Hash       string `json:"hash"`
--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"github.com/nomadcoders/nomadcoin/cli"
-	"github.com/nomadcoders/nomadcoin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	defer db.Close()
-	cli.Start()
+	difficutly := 6
+	target := strings.Repeat("0", difficutly)
+	nonce := 1
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
+		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
+		if strings.HasPrefix(hash, target) {
+			return
+		} else {
+			nonce++
+		}
+	}
 }
