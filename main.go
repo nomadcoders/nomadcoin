193865c016fdcfa226332da4be412f02b87b58b9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-06-27 15:57:15 +0900

    9.6 Persit Wallet

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/nomadcoders/nomadcoin/wallet"
 
 func main() {
-	wallet.Start()
+	wallet.Wallet()
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,7 +2,16 @@ package wallet
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
 	"os"
+
+	"github.com/nomadcoders/nomadcoin/utils"
+)
+
+const (
+	fileName string = "nomadcoin.wallet"
 )
 
 type wallet struct {
@@ -12,16 +21,33 @@ type wallet struct {
 var w *wallet
 
 func hasWalletFile() bool {
-	_, err := os.Stat("nomadcoin.wallet")
+	_, err := os.Stat(fileName)
 	return !os.IsNotExist(err)
+}
+
+func createPrivKey() *ecdsa.PrivateKey {
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	utils.HandleErr(err)
+	return privKey
+}
+
+func persistKey(key *ecdsa.PrivateKey) {
+	bytes, err := x509.MarshalECPrivateKey(key)
+	utils.HandleErr(err)
+	err = os.WriteFile(fileName, bytes, 0644)
+	utils.HandleErr(err)
 }
 
 func Wallet() *wallet {
 	if w == nil {
+		w = &wallet{}
 		if hasWalletFile() {
 			// yes -> restore from file
+		} else {
+			key := createPrivKey()
+			persistKey(key)
+			w.privateKey = key
 		}
-		// no -> create prv key, save to file
 	}
 	return w
 }
