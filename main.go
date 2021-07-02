b2fabf4471335cf12ec6c4d7c151f8bf2ebc933d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-02 19:00:13 +0900

    10.1 Why Go Routines

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"github.com/nomadcoders/nomadcoin/cli"
-	"github.com/nomadcoders/nomadcoin/db"
+	"fmt"
+	"time"
 )
 
+func countToTen(name string) {
+	for i := range [10]int{} {
+		fmt.Println(i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	defer db.Close()
-	cli.Start()
+	go countToTen("first")
+	go countToTen("second")
+	for {
+	}
 }
