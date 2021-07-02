9d873a85101bb390f9fe03601a8c0ba52101cfe4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-02 19:58:35 +0900

    10.3 Raead, Receive and Close

--- main.go
+++ main.go
@@ -5,20 +5,28 @@ import (
 	"time"
 )
 
-func countToTen(c chan int) {
+func countToTen(c chan<- int) {
 	for i := range [10]int{} {
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 		fmt.Printf("sending %d\n", i)
 		c <- i
+	}
+	close(c)
+}
+
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if !ok {
+			fmt.Println("we are done.")
+			break
+		}
+		fmt.Printf("received %d\n", a)
 	}
 }
 
 func main() {
 	c := make(chan int)
 	go countToTen(c)
-	for {
-		a := <-c
-		fmt.Printf("received %d\n", a)
-	}
-
+	receive(c)
 }
