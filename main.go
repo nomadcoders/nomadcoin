1b76496de838ad653d4781245f1872fc5b5c0d04
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-02 20:36:09 +0900

    10.4 Buffered Channels

--- main.go
+++ main.go
@@ -5,28 +5,29 @@ import (
 	"time"
 )
 
-func countToTen(c chan<- int) {
+func send(c chan<- int) {
 	for i := range [10]int{} {
-		time.Sleep(1 * time.Second)
-		fmt.Printf("sending %d\n", i)
+		fmt.Printf(">> sending %d <<\n", i)
 		c <- i
+		fmt.Printf(">> sent %d <<\n", i)
 	}
 	close(c)
 }
 
 func receive(c <-chan int) {
 	for {
+		time.Sleep(10 * time.Second)
 		a, ok := <-c
 		if !ok {
 			fmt.Println("we are done.")
 			break
 		}
-		fmt.Printf("received %d\n", a)
+		fmt.Printf("|| received %d ||\n", a)
 	}
 }
 
 func main() {
-	c := make(chan int)
-	go countToTen(c)
+	c := make(chan int, 10)
+	go send(c)
 	receive(c)
 }
