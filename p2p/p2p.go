3bd34185ee00ae68a5057e814af16456665d7a0b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-04 17:28:42 +0900

    10.8 Connections

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,14 +1,13 @@
 package p2p
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/nomadcoders/nomadcoin/utils"
 )
 
+var conns []*websocket.Conn
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
@@ -16,15 +15,17 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 		return true
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
+	conns = append(conns, conn)
 	utils.HandleErr(err)
 	for {
 		_, p, err := conn.ReadMessage()
 		if err != nil {
 			break
 		}
-		fmt.Printf("Just got:%s\n\n", p)
-		time.Sleep(5 * time.Second)
-		message := fmt.Sprintf("New message: %s", p)
-		utils.HandleErr(conn.WriteMessage(websocket.TextMessage, []byte(message)))
+		for _, aConn := range conns {
+			if aConn != conn {
+				utils.HandleErr(aConn.WriteMessage(websocket.TextMessage, p))
+			}
+		}
 	}
 }
