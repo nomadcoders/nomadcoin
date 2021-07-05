f1a8a68305b5818b460a7409fa03b70340182de1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-05 16:44:19 +0900

    10.14 Inbox

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -20,16 +20,16 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, ip, openPort)
+	peer := initPeer(conn, ip, openPort)
 	time.Sleep(20 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
+	peer.inbox <- []byte("Hello from 3000!")
 }
 
 func AddPeer(address, port, openPort string) {
 	// Port :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	initPeer(conn, address, port)
+	peer := initPeer(conn, address, port)
 	time.Sleep(10 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
+	peer.inbox <- []byte("Hello from 4000!")
 }
--- p2p/peer.go
+++ p2p/peer.go
@@ -9,7 +9,8 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn *websocket.Conn
+	conn  *websocket.Conn
+	inbox chan []byte
 }
 
 func (p *peer) read() {
@@ -23,12 +24,21 @@ func (p *peer) read() {
 	}
 }
 
+func (p *peer) write() {
+	for {
+		m := <-p.inbox
+		p.conn.WriteMessage(websocket.TextMessage, m)
+	}
+}
+
 func initPeer(conn *websocket.Conn, address, port string) *peer {
 	key := fmt.Sprintf("%s:%s", address, port)
 	p := &peer{
 		conn,
+		make(chan []byte),
 	}
 	go p.read()
+	go p.write()
 	Peers[key] = p
 	return p
 }
