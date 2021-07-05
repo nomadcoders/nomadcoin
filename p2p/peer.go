df13f15361e2ad138999fd4fbc479b24b39ff453
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-05 20:54:09 +0900

    10.23 Recap

--- p2p/peer.go
+++ p2p/peer.go
@@ -65,6 +65,8 @@ func (p *peer) write() {
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
+	Peers.m.Lock()
+	defer Peers.m.Unlock()
 	key := fmt.Sprintf("%s:%s", address, port)
 	p := &peer{
 		conn:    conn,
