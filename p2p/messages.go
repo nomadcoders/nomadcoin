cbb1cdc8cd64e36e31f9d3c8d3825041735ea2a3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-05 19:51:27 +0900

    10.19 Messages

---
+++ p2p/messages.go
@@ -0,0 +1,14 @@
+package p2p
+
+type MessageKind int
+
+const (
+	MessageNewestBlock MessageKind = iota
+	MessageAllBlocksRequest
+	MessageAllBlocksResponse
+)
+
+type Message struct {
+	Kind    MessageKind
+	Payload []byte
+}
--- p2p/peer.go
+++ p2p/peer.go
@@ -44,11 +44,12 @@ func (p *peer) close() {
 func (p *peer) read() {
 	defer p.close()
 	for {
-		_, m, err := p.conn.ReadMessage()
+		m := Message{}
+		err := p.conn.ReadJSON(&m)
 		if err != nil {
 			break
 		}
-		fmt.Printf("%s", m)
+		fmt.Print(m.Kind)
 	}
 }
 
