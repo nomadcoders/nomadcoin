a6c6efcf7fef9f86683615fb0ca177bf3871d903
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-10 19:00:27 +0900

    11.14 Blockchain Test

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -3,6 +3,8 @@ package blockchain
 import (
 	"reflect"
 	"testing"
+
+	"github.com/nomadcoders/nomadcoin/utils"
 )
 
 func TestCreateBlock(t *testing.T) {
@@ -12,4 +14,32 @@ func TestCreateBlock(t *testing.T) {
 	if reflect.TypeOf(b) != reflect.TypeOf(&Block{}) {
 		t.Error("createBlock() should return an instance of a block")
 	}
+}
+
+func TestFindBlock(t *testing.T) {
+	t.Run("Block not found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				return nil
+			},
+		}
+		_, err := FindBlock("xx")
+		if err == nil {
+			t.Error("The block should not be found.")
+		}
+	})
+	t.Run("Block is found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height: 1,
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		block, _ := FindBlock("xx")
+		if block.Height != 1 {
+			t.Error("Block should be found.")
+		}
+	})
 }
--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"sync"
+	"testing"
+
+	"github.com/nomadcoders/nomadcoin/utils"
+)
+
 type fakeDB struct {
 	fakeLoadChain func() []byte
 	fakeFindBlock func() []byte
@@ -14,3 +21,30 @@ func (f fakeDB) LoadChain() []byte {
 func (fakeDB) SaveBlock(hash string, data []byte) {}
 func (fakeDB) SaveChain(data []byte)              {}
 func (fakeDB) DeleteAllBlocks()                   {}
+
+func TestBlockchain(t *testing.T) {
+	t.Run("Should create blockchain", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				return nil
+			},
+		}
+		bc := Blockchain()
+		if bc.Height != 1 {
+			t.Error("Blockchain() should create a blockchain.")
+		}
+	})
+	t.Run("Should restore blockchain", func(t *testing.T) {
+		once = *new(sync.Once)
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				bc := &blockchain{Height: 2, NewestHash: "xxx", CurrentDifficulty: 1}
+				return utils.ToBytes(bc)
+			},
+		}
+		bc := Blockchain()
+		if bc.Height != 2 {
+			t.Errorf("Blockchain() should restore a blockchain with a height of %d, got %d", 2, bc.Height)
+		}
+	})
+}
