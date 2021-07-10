95d98414be501340068649ac069c9f58c271c35c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-10 19:30:29 +0900

    11.15 Blocks Test

--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"reflect"
 	"sync"
 	"testing"
 
@@ -47,4 +48,68 @@ func TestBlockchain(t *testing.T) {
 			t.Errorf("Blockchain() should restore a blockchain with a height of %d, got %d", 2, bc.Height)
 		}
 	})
+}
+
+func TestBlocks(t *testing.T) {
+	fakeBlocks := 0
+	dbStorage = fakeDB{
+		fakeFindBlock: func() []byte {
+			var b *Block
+			if fakeBlocks == 0 {
+				b = &Block{
+					Height:   2,
+					PrevHash: "x",
+				}
+			}
+			if fakeBlocks == 1 {
+				b = &Block{
+					Height: 1,
+				}
+			}
+			fakeBlocks++
+			return utils.ToBytes(b)
+		},
+	}
+	bc := &blockchain{}
+	blocks := Blocks(bc)
+	if reflect.TypeOf(blocks) != reflect.TypeOf([]*Block{}) {
+		t.Error("Blocks() should return a slice of blocks")
+	}
+
+}
+
+func TestFindTx(t *testing.T) {
+	t.Run("Tx not found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height:       2,
+					Transactions: []*Tx{},
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		tx := FindTx(&blockchain{NewestHash: "x"}, "test")
+		if tx != nil {
+			t.Error("Tx should be not found.")
+		}
+	})
+	t.Run("Tx should be found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height: 2,
+					Transactions: []*Tx{
+						{ID: "test"},
+					},
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		tx := FindTx(&blockchain{NewestHash: "x"}, "test")
+		if tx == nil {
+			t.Error("Tx should be found.")
+		}
+	})
+
 }
