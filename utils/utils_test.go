e8dd17c9335f84f326c3bfc1c5d0539b802cfc14
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-10 14:26:02 +0900

    11.3 Coverage

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/hex"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -29,4 +30,13 @@ func ExampleHash() {
 	x := Hash(s)
 	fmt.Println(x)
 	// Output: e005c1d727f7776a57a661d61a182816d8953c0432780beeae35e337830b1746
+}
+
+func TestToBytes(t *testing.T) {
+	s := "test"
+	b := ToBytes(s)
+	k := reflect.TypeOf(b).Kind()
+	if k != reflect.Slice {
+		t.Errorf("ToBytes should return a slice of bytes got %s", k)
+	}
 }
